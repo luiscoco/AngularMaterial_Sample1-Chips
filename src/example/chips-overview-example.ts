ea1db8231a812f5c75e94b578159aedab50fb9f8
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-17 11:00:11 +0200

    first commit

---
+++ src/example/chips-overview-example.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
+
+/**
+ * @title Basic chips
+ */
+@Component({
+  selector: 'chips-overview-example',
+  templateUrl: 'chips-overview-example.html',
+  standalone: true,
+  imports: [MatChipsModule],
+})
+export class ChipsOverviewExample {}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
