9140e3402ec23ae02bbfd8ddc0327a4fa4a39da2
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-26 08:30:21 +0200

    environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://example.com/api', // API endpoint for production environment
+  // Add more environment-specific variables here
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://example.com/api', // API endpoint for production environment
+    // Add more environment-specific variables here
+  };
+  
